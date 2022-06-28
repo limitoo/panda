50f00cd7dc1938026c8a78e0eeedc071dcbbef9a
Richard <richard@newborntown.com>
2022-06-28 19:54:54 +0800

    Fixed bug get id.

--- src/controllers/news.rs
+++ src/controllers/news.rs
@@ -1,6 +1,6 @@
 use sqlx::{Error, Pool, MySql};
 
-use crate::model::news::{NewsLists, Minidetails, LasterLists};
+use crate::model::news::{NewsLists, LasterLists};
 
 
 pub async fn sql_news_lists(pool: &Pool<MySql>) ->  Result<Vec<NewsLists>, Error> {
@@ -12,21 +12,12 @@ pub async fn sql_news_lists(pool: &Pool<MySql>) ->  Result<Vec<NewsLists>, Error
 	return Ok(records);
 }
 
-pub async fn sql_news_id(pool: &Pool<MySql>, id: i32) ->  Result<Minidetails, Error> {
-	sqlx::query_as::<_, Minidetails>(
+pub async fn sql_news_id(pool: &Pool<MySql>, id: i32) ->  Result<LasterLists, Error> {
+	sqlx::query_as::<_, LasterLists>(
 		r#"
-			SELECT id, news_id, src, create_time, content  FROM details WHERE id = ?
-		"#,
-		)
-		.bind(id)
-		.fetch_one(pool).await
-}
-
-// select ny.id, nyd.content from news ny, details nyd where ny.id = nyd.news_id order by ny.create_time desc limit 10;
-pub async fn sql_laster_100(pool: &Pool<MySql>, num: i32) ->  Result<Vec<LasterLists>, Error> {
-	let records:Vec<LasterLists> = sqlx::query_as::<_, LasterLists>(
-		r#"
-		select ny.href,
+		select 
+		ny.id,
+		ny.href,
 		ny.img_url,
 		ny.load_img,
 		ny.title,
@@ -37,7 +28,32 @@ pub async fn sql_laster_100(pool: &Pool<MySql>, num: i32) ->  Result<Vec<LasterL
 		ny.create_time,
 		ny.menu,
 		ny.href_hash,
-		nyd.id,
+		nyd.local_src,
+		nyd.src,
+		nyd.content from news ny, details nyd where ny.id = nyd.news_id and ny.id = ?;
+		"#,
+		)
+		.bind(id)
+		.fetch_one(pool).await
+}
+
+// select ny.id, nyd.content from news ny, details nyd where ny.id = nyd.news_id order by ny.create_time desc limit 10;
+pub async fn sql_laster_100(pool: &Pool<MySql>, num: i32) ->  Result<Vec<LasterLists>, Error> {
+	let records:Vec<LasterLists> = sqlx::query_as::<_, LasterLists>(
+		r#"
+		select 
+		ny.id,
+		ny.href,
+		ny.img_url,
+		ny.load_img,
+		ny.title,
+		ny.country,
+		ny.source,
+		ny.description,
+		ny.status,
+		ny.create_time,
+		ny.menu,
+		ny.href_hash,
 		nyd.local_src,
 		nyd.src,
 		nyd.content from news ny, details nyd where ny.id = nyd.news_id order by ny.create_time desc limit ?;
